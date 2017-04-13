1da6bce9c808b38fbc071d87b13e00cf02ddcb1d
charlesdrews <charlesdrews@gmail.com>
2017-04-13 19:10:56 -0400

    Update colors & icons; setup navigation

--- DontForget/app/src/main/java/com/charlesdrews/dontforget/MainActivity.java
+++ DontForget/app/src/main/java/com/charlesdrews/dontforget/MainActivity.java
@@ -1,17 +1,13 @@
 package com.charlesdrews.dontforget;
 
-import android.graphics.Color;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.AppBarLayout;
 import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.content.ContextCompat;
-import android.support.v4.view.WindowCompat;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -19,28 +15,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener,
+        BottomNavigationView.OnNavigationItemSelectedListener,
+        BaseFragment.OnFragmentInteractionListener {
 
+    private static final int INVALID_SELECTION = -1;
     private static final int DAILY_SUMMARY = 0;
     private static final int WEATHER = 1;
     private static final int TASKS = 2;
     private static final int BIRTHDAYS = 3;
+    private static final int SETTINGS = 4;
 
-    private int mSelectedFragment = -1;
+    private int mLastSelection = DAILY_SUMMARY;
+    private NavigationView mNavDrawer;
+    private DrawerLayout mNavDrawerLayout;
+    private BottomNavigationView mNavBottom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-//            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorAccentDark));
-        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -51,24 +51,30 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mNavDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
+                this, mNavDrawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mNavDrawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navDrawer = (NavigationView) findViewById(R.id.nav_view);
-        navDrawer.setNavigationItemSelectedListener(this);
+        mNavDrawer = (NavigationView) findViewById(R.id.nav_view);
+        mNavDrawer.setNavigationItemSelectedListener(this);
 
-        BottomNavigationView navBottom = (BottomNavigationView) findViewById(R.id.bottom_nav);
-        navBottom.setOnNavigationItemSelectedListener(getBottomNavSelectListener());
+        mNavBottom = (BottomNavigationView) findViewById(R.id.bottom_nav);
+        mNavBottom.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                return handleNavSelection(item.getItemId());
+            }
+        });
+
+        replaceFragment(getSelectedFragment());
     }
 
     @Override
     public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
+        if (mNavDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+            mNavDrawerLayout.closeDrawer(GravityCompat.START);
         } else {
             super.onBackPressed();
         }
@@ -82,80 +88,85 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_settings:
-                showSettings();
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return handleNavSelection(item.getItemId());
     }
 
+    // Sufficient to implement both NavigationView.OnNavigationItemSelectedListener
+    // and BottomNavigationView.OnNavigationItemSelectedListener
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_summary:
-                showDailySummary();
-                break;
-            case R.id.nav_weather:
-                showWeather();
-                break;
-            case R.id.nav_tasks:
-                showTasks();
-                break;
-            case R.id.nav_birthdays:
-                showBirthdays();
-                break;
-            case R.id.nav_settings:
-                showSettings();
-                break;
-        }
+        return handleNavSelection(item.getItemId());
+    }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+    private boolean handleNavSelection(int selectionResId) {
+        int newSelection = getSelectionIndex(selectionResId);
+
+        if (newSelection == INVALID_SELECTION) {
+            return false;
+        } else if (newSelection == SETTINGS) {
+            Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+        } else if (newSelection != mLastSelection) {
+            mLastSelection = newSelection;
+            mNavDrawer.getMenu().getItem(mLastSelection).setChecked(true);
+            mNavBottom.getMenu().getItem(mLastSelection).setChecked(true);
+            replaceFragment(getSelectedFragment());
+        }
+        mNavDrawerLayout.closeDrawer(GravityCompat.START);
         return true;
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener getBottomNavSelectListener() {
-        return new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.nav_bottom_summary:
-                        showDailySummary();
-                        return true;
-                    case R.id.nav_bottom_weather:
-                        showWeather();
-                        return true;
-                    case R.id.nav_bottom_tasks:
-                        showTasks();
-                        return true;
-                    case R.id.nav_bottom_birthdays:
-                        showBirthdays();
-                        return true;
-                }
-                return false;
-            }
-        };
+    private int getSelectionIndex(int selectionResId) {
+        switch (selectionResId) {
+            case R.id.nav_drawer_summary:
+            case R.id.nav_bottom_summary:
+                return DAILY_SUMMARY;
+
+            case R.id.nav_drawer_weather:
+            case R.id.nav_bottom_weather:
+                return WEATHER;
+
+            case R.id.nav_drawer_tasks:
+            case R.id.nav_bottom_tasks:
+                return TASKS;
+
+            case R.id.nav_drawer_birthdays:
+            case R.id.nav_bottom_birthdays:
+                return BIRTHDAYS;
+
+            case R.id.nav_drawer_settings:
+            case R.id.action_settings:
+                return SETTINGS;
+
+            default:
+                return INVALID_SELECTION;
+        }
     }
 
-    private void showDailySummary() {
-        Toast.makeText(this, "Daily Summary", Toast.LENGTH_SHORT).show();
+    private Fragment getSelectedFragment() {
+        switch (mLastSelection) {
+            case DAILY_SUMMARY:
+                return BaseFragment.newInstance(R.drawable.ic_summary, "");
+            case WEATHER:
+                return BaseFragment.newInstance(R.drawable.ic_weather, "");
+            case TASKS:
+                return BaseFragment.newInstance(R.drawable.ic_tasks, "");
+            case BIRTHDAYS:
+                return BaseFragment.newInstance(R.drawable.ic_birthdays, "");
+            default:
+                return null;
+        }
     }
 
-    private void showWeather() {
-        Toast.makeText(this, "Weather", Toast.LENGTH_SHORT).show();
+    private void replaceFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+        }
     }
 
-    private void showTasks() {
-        Toast.makeText(this, "Tasks", Toast.LENGTH_SHORT).show();
-    }
+    @Override
+    public void onFragmentInteraction(Uri uri) {
 
-    private void showBirthdays() {
-        Toast.makeText(this, "Birthdays", Toast.LENGTH_SHORT).show();
-    }
-
-    private void showSettings() {
-        Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
     }
 }
