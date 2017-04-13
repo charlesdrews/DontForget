b86111097537527c28ea734512f958c7b8d23828
charlesdrews <charlesdrews@gmail.com>
2017-04-13 01:54:54 -0400

    Set up colors and navigation views

--- DontForget/app/src/main/java/com/charlesdrews/dontforget/MainActivity.java
+++ DontForget/app/src/main/java/com/charlesdrews/dontforget/MainActivity.java
@@ -1,8 +1,15 @@
 package com.charlesdrews.dontforget;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.AppBarLayout;
+import android.support.design.widget.BottomNavigationView;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.WindowCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,9 +19,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private static final int DAILY_SUMMARY = 0;
+    private static final int WEATHER = 1;
+    private static final int TASKS = 2;
+    private static final int BIRTHDAYS = 3;
+
+    private int mSelectedFragment = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +37,10 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            getWindow().setStatusBarColor(ContextCompat.getColor(this, R.color.colorAccentDark));
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,8 +57,11 @@ public class MainActivity extends AppCompatActivity
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
+        NavigationView navDrawer = (NavigationView) findViewById(R.id.nav_view);
+        navDrawer.setNavigationItemSelectedListener(this);
+
+        BottomNavigationView navBottom = (BottomNavigationView) findViewById(R.id.bottom_nav);
+        navBottom.setOnNavigationItemSelectedListener(getBottomNavSelectListener());
     }
 
     @Override
@@ -54,48 +76,86 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                showSettings();
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_summary:
+                showDailySummary();
+                break;
+            case R.id.nav_weather:
+                showWeather();
+                break;
+            case R.id.nav_tasks:
+                showTasks();
+                break;
+            case R.id.nav_birthdays:
+                showBirthdays();
+                break;
+            case R.id.nav_settings:
+                showSettings();
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener getBottomNavSelectListener() {
+        return new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.nav_bottom_summary:
+                        showDailySummary();
+                        return true;
+                    case R.id.nav_bottom_weather:
+                        showWeather();
+                        return true;
+                    case R.id.nav_bottom_tasks:
+                        showTasks();
+                        return true;
+                    case R.id.nav_bottom_birthdays:
+                        showBirthdays();
+                        return true;
+                }
+                return false;
+            }
+        };
+    }
+
+    private void showDailySummary() {
+        Toast.makeText(this, "Daily Summary", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showWeather() {
+        Toast.makeText(this, "Weather", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showTasks() {
+        Toast.makeText(this, "Tasks", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showBirthdays() {
+        Toast.makeText(this, "Birthdays", Toast.LENGTH_SHORT).show();
+    }
+
+    private void showSettings() {
+        Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
     }
 }
